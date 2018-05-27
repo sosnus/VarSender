8e9b2e00427fcc19764a787cfd583cd206bc2d74
Sosnus <sosnus15@gmail.com>
2018-05-28 00:03:58 +0200

    serialEvent method remove to lib

--- VarSenderLib.cpp
+++ VarSenderLib.cpp
@@ -26,3 +26,30 @@ void VarSender::setVar(uint8_t var, uint8_t value)
   values[var]=value;
 }
 
+
+
+void serialEvent()
+{ //new char on serialport
+  uint8_t destination, value;
+  while (Serial.available())
+  {
+    //start trans [!] variabl. [A-Z] value [100-199]
+    if ((char)Serial.read() == '!') //start transmission
+    {
+      do
+      {
+        destination = Serial.read(); //-64;
+      } while (destination < 0);
+      destination -= 65;
+      do
+      {
+        value = Serial.read(); //-100;
+      } while (value < 0);
+      value -= 100;
+      v.setVar(destination,value);
+      //values[destination] = value;
+    }
+  }
+}
+
+
