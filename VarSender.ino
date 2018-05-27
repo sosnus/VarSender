25ee4f7468eda9b438d7bb54d9c0e94d9e5dede9
Sosnus <sosnus15@gmail.com>
2018-05-27 22:25:46 +0200

    init INO file

--- VarSender.ino
+++ VarSender.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+
+uint8_t values[100]; //from A to Z
+
+
+void setup()
+{
+  Serial.begin(9600);
+
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 
 }
+
+
+void serialEvent()
+{ //new char on serialport
+  uint8_t destination, value;
+  while (Serial.available())
+  {
+    //start trans [!] variabl. [A-Z] value [100-199]
+    if ((char)Serial.read() == '!') //start transmission
+    {
+      do
+      {
+        destination = Serial.read(); //-64;
+      } while (destination < 0);
+      destination -= 65;
+      do
+      {
+        value = Serial.read(); //-100;
+      } while (value < 0);
+      value -= 100;
+      values[destination] = value;
+    }
+  }
+}
+
